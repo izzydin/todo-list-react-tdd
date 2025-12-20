a721a749118119dd9eb99a9575040dc83fd0378d
Javier Stambuk <blackcatundertheceiling@gmail.com>
2025-12-20 14:32:06 -0400

    refactor: extract todo form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { TodoForm } from './features/todos/components/TodoForm'
 
 function App() {
   const [todos, setTodos] = useState<string[]>([])
-  const [inputValue, setInputValue] = useState('')
 
-  const handleAdd = () => {
-    if (!inputValue) return
-    setTodos([...todos, inputValue])
-    setInputValue('')
+  const handleAdd = (text: string) => {
+    setTodos([...todos, text])
   }
 
   return (
     <div className="app-container">
       <h1>Todo List</h1>
-      <div className="input-group">
-        <input
-          type="text"
-          placeholder="Add a new task"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button onClick={handleAdd}>Add</button>
-      </div>
+      <TodoForm onSubmit={handleAdd} />
       {todos.length === 0 && <p>No tasks yet. Add one!</p>}
       <ul>
         {todos.map((todo, index) => (
